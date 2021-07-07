6e64e69a6ff8b187a5a3ac2ae3629a419d834138
Raghed Idris <raghed.edriss@isae.edu.lb>
2021-07-08 02:21:38 +0300

    question3 done

--- tp5/question3/Tests.java
+++ tp5/question3/Tests.java
@@ -4,7 +4,7 @@ import java.util.Set;
 
 public class Tests extends junit.framework.TestCase {
 
-    public void test1(question3.Factory/* à compléter */f) throws Exception {
+    public void test1(question3.Factory<Set> f) throws Exception {
         Set<Integer> set = f.create();
         for (int i = 20; i > 0; i--)
             set.add(i);
@@ -20,5 +20,13 @@ public class Tests extends junit.framework.TestCase {
             fail(" exception inattendue : " + e.getMessage());
         }
     }
-
+    
+    public void testCompare(){
+        question3.Factory<Set> f1= new TreeSetFactory<String>();
+        question3.Factory<Set> f2= new TreeSetFactory<String>();
+        question3.Factory<Set> f3= new TreeSetFactory<String>();
+        Set<String> set=f1.create();
+        f2.create();
+        f3.create();
+    }
 }
